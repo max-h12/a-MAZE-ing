f4e522909e9d61016c2ccde9b330234119001c57
maxdg99 <Maxdg99@gmail.com>
2018-10-18 23:01:15 -0400

    This sorta maybe kinda fixed cheese falling outside maze

--- mazeScript.js
+++ mazeScript.js
@@ -2,7 +2,6 @@
 *Add rotation to image based on arrows
 */
 
-
 //define canvas
 var canvasMaze = document.getElementById('canvasMaze');
 var cM = canvasMaze.getContext("2d");
@@ -11,7 +10,6 @@ var canvasPlayer = document.getElementById('canvasPlayer');
 var cP = canvasPlayer.getContext("2d");
 
 var mazeDrawn = false;
-
 
 //set canvas size to size of webpage
 cM.canvas.width = window.innerWidth;
@@ -40,6 +38,10 @@ var yPlayer = 0;
 var mazeWidth = 20;
 var mazeHeight = 20;
 
+var actualMazeWidth = (mazeWidth - 1) * sz;
+var actualMazeHeight = (mazeHeight - 1) * sz;
+
+
 //position of goal image.  530, 35
 var endX = 10;
 var endY = 10;
@@ -49,56 +51,78 @@ var left = '37';
 var right = '39';
 var up = '38';
 var down = '40';
+var plus = '187';
+var minus = '189';
 
 
-var marginX = ((cP.canvas.width) - (mazeWidth*sz))/2; //(canvasMaze.width - (mazeWidth * sz)) / 2 ;
-var marginY = ((cP.canvas.height) - (mazeHeight*sz))/2;//(canvasMaze.heigth - (mazeHeight * sz)) / 2 ;
+console.log("Width: " + actualMazeWidth);
+
+
+var marginX = ((cP.canvas.width) - (mazeWidth*sz))/2;
+var marginY = ((cP.canvas.height) - (mazeHeight*sz))/2;
 
 
 document.onkeydown = checkKey;
+document.onmousemove = function()
+{
+  console.log("P:" + event.clientX + "," + event.clientY);
+}
 
 
 /* Draws all images that are fixed within canvas
 *This includeds the main board and goal */
 function drawImage(){
-        if( Math.abs(yPlayer - endY) <= 5 && Math.abs(xPlayer - endX) <= 5)
+        if( Math.abs(yPlayer - endY) <= 5 && Math.abs(xPlayer - endX) <= 5 && xPlayer>0 && yPlayer>0)
         {
+          console.log("test");
             cP.clearRect(0, 0, canvasPlayer.width, canvasPlayer.height);
             cM.clearRect(0, 0, canvasMaze.width, canvasMaze.height);
             document.getElementById("title").innerHTML = "YOU WON!!! YOU ARE A-MAZE-ING!!!";
             var winner = new Image();
             winner.src = "https://imagesvc.timeincapp.com/v3/mm/image?url=https%3A%2F%2Fimages.hellogiggles.com%2Fuploads%2F2016%2F12%2F09032937%2FSuccess-Kid.jpg&w=700&c=sc&poi=face&q=85";
             cP.drawImage(winner,(canvasPlayer.width-winner.width)/2, (canvasPlayer.height-winner.height)/2);
-            return true;
         }
         else
         {
+
             cP.clearRect(0, 0, canvasPlayer.width, canvasPlayer.height);
             cP.drawImage(goal, endX, endY, playerSize, playerSize);
             cP.drawImage(player, xPlayer, yPlayer, playerSize, playerSize);
         }
 }
 
-function randomVal(max, min)
-{
-    return Math.floor(Math.random() * (max - min) ) + min;
-}
-
+/*Places the goal random in the maze*/
 function randomEnd(){  //0 is end, 1 is player
 
-    var tempEndX;
-    var tempEndY;
+  console.log(marginX);
+  //I have no idea how to randomly place the chese
+  var maxX =  cP.canvas.width*.5 + (actualMazeWidth*.5);
+  var minX = cP.canvas.width*.5 - (actualMazeWidth * .5);
+  var maxY =  cP.canvas.height*.5 + (actualMazeHeight * .5);
+  var minY =  cP.canvas.height*.5 - (actualMazeHeight * .5);
+  console.log(minX +  " to " +  maxX);
+  console.log(minY +  " to " +  maxY);
+  tempEndX = Math.floor(Math.random()*(maxX-minX))+minX;
+  tempEndY = Math.floor(Math.random()*(maxY-minY)) + minY;
 
-    while(canMoveThere(tempEndX,tempEndY) == 1)
+    while(!canMoveThere(tempEndX,tempEndY))
     {
-        tempEndX = Math.floor(Math.random()*((mazeWidth*sz) + marginX)) + marginX;
-        tempEndY = Math.floor(Math.random()*((mazeHeight*sz) + marginY)) + marginY;
+        console.log("run");
+        maxX = cP.canvas.width*.5 + (actualMazeWidth*.5) - sz;
+        minX = cP.canvas.width*.5 - (actualMazeWidth * .5) + sz;
+        maxY =  cP.canvas.height*.5 + (actualMazeHeight * .5) - sz;
+        minY =  cP.canvas.height*.5 - (actualMazeHeight * .5) + sz;
+        console.log(minX +  " to " +  maxX)
+        tempEndX = Math.floor(Math.random()*(maxX-minX))+minX;
+        tempEndY = Math.floor(Math.random()*(maxY-minY)) + minY;
+        console.log(tempEndX);
+        console.log(tempEndY);
     }
 
       endX = tempEndX;
       endY = tempEndY;
 }
-
+/*Draw the main maze*/
 function drawMaze(){
 
 
@@ -126,25 +150,22 @@ function drawMaze(){
 *can move to any given x and y */
 function canMoveThere(x, y)
 {
-    var collision = 0;
+    var collision = true;
     //x > marginX+sz && y > marginY+sz
-    if( x < (mazeWidth*sz)-sz  && y < (mazeHeight*sz)-sz){
+    if(x>0 && y>0){
         var color = cM.getImageData(x, y, playerSize, playerSize);
         var p = color.data;
         var n = p.length;
         for(var i = 0; i<n; i+=4){
             if(p[i] === 0)
             {
-                 collision = 1;
+                 collision = false;
             }
         }
-    }
-    else{
-        collision = 1;
-    }
+      }
     return collision;
-
 }
+
 
 
 
@@ -162,6 +183,7 @@ function checkKey(e)
             xPlayer = xPlayer--;
             if(canMoveThere(xPlayer, yPlayer) == 0)
             {
+
               drawImage();
             }
             else {
@@ -175,13 +197,13 @@ function checkKey(e)
         e.preventDefault();
         for(var i = 0; i<speed; i++)
         {
-            xPlayer = xPlayer--;
-            if(canMoveThere(xPlayer, yPlayer) == 0)
+            xPlayer++;
+            if(canMoveThere(xPlayer, yPlayer))
             {
               drawImage();
             }
             else {
-              xPlayer++;
+              xPlayer--;
             }
         }
     }
@@ -190,13 +212,13 @@ function checkKey(e)
         e.preventDefault();
         for(var i = 0; i<speed; i++)
         {
-            xPlayer = xPlayer--;
-            if(canMoveThere(xPlayer, yPlayer) == 0)
+            yPlayer--;
+            if(canMoveThere(xPlayer, yPlayer))
             {
               drawImage();
             }
             else {
-              xPlayer++;
+              yPlayer++;
             }
         }
     }
@@ -205,18 +227,18 @@ function checkKey(e)
         e.preventDefault();
         for(var i = 0; i<speed; i++)
         {
-            xPlayer = xPlayer--;
-            if(canMoveThere(xPlayer, yPlayer) == 0)
+            yPlayer--;
+            if(canMoveThere(xPlayer, yPlayer))
             {
               drawImage();
             }
             else {
-              xPlayer++;
+              yPlayer++;
             }
         }
     }
 
-    if(e.keyCode == 187)
+    if(e.keyCode == plus)
     {
         speed+= 1;
         if(speed > maxSpeed)
@@ -224,7 +246,7 @@ function checkKey(e)
           speed -= 1;
         }
     }
-    if(e.keyCode == 189)
+    if(e.keyCode == minus)
     {
         speed-= 1;
         if(speed > minSpeed)
@@ -233,9 +255,7 @@ function checkKey(e)
         }
     }
 
-
     document.getElementById("speed").innerHTML = "SPEED: " + speed;
-
 
     if(!mazeDrawn)
     {
