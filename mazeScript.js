210f6adacf53168fac7af04c39c5502c3768d64e
maxdg99 <Maxdg99@gmail.com>
2018-10-19 15:00:51 -0400

    Fixed random goal placement.  Fixed Winning sequence

--- mazeScript.js
+++ mazeScript.js
@@ -44,7 +44,7 @@ var actualMazeHeight = (mazeHeight - 1) * sz;
 var amountOfTime = 5;
 
 var wonGame = false;
-var numberOfItems = 3;
+var numberOfItems = 2;
 var numberCaught = 0;
 
 var endX=[];
@@ -57,6 +57,11 @@ var up = '38';
 var down = '40';
 var plus = '187';
 var minus = '189';
+
+var maxX =  cP.canvas.width*.5 + (actualMazeWidth*.5);
+var minX = cP.canvas.width*.5 - (actualMazeWidth * .5);
+var maxY =  cP.canvas.height*.5 + (actualMazeHeight * .5);
+var minY =  cP.canvas.height*.5 - (actualMazeHeight * .5);
 
 var marginX = ((cP.canvas.width) - (mazeWidth*sz))/2;
 var marginY = ((cP.canvas.height) - (mazeHeight*sz))/2;
@@ -71,55 +76,45 @@ document.onmousemove = function()
 /* Draws all images that are fixed within canvas
 *This includeds the main board and goal */
 function drawImage(){
-        if( Math.abs(yPlayer - endY) <= 5 && Math.abs(xPlayer - endX) <= 5 && xPlayer>0 && yPlayer>0)
-        {
-          if(numberOfItems == numberCaught){
-            wonGame = true;
-            cP.clearRect(0, 0, canvasPlayer.width, canvasPlayer.height);
-            cM.clearRect(0, 0, canvasMaze.width, canvasMaze.height);
-            document.getElementById("title").innerHTML = "YOU WON!!! YOU ARE A-MAZE-ING!!!";
-            var winner = new Image();
-            winner.src = "win.webp";
-            cP.drawImage(winner,(canvasPlayer.width-winner.width)/2, (canvasPlayer.height-winner.height)/2);
+    for(var n = 0; n<numberOfItems; n++){
+          if( Math.abs(yPlayer - endY[n]) <= 10 && Math.abs(xPlayer - endX[n]) <= 10 && xPlayer>0 && yPlayer>0)
+          {
+              numberCaught++;
+              endX[n] = -100;
+              endY[n] = -100;
+              console.log(numberCaught)
           }
-          else {
-            numberCaught++;
+          else
+          {
+              cP.clearRect(0, 0, canvasPlayer.width, canvasPlayer.height);
+              for(var i = 0; i<numberOfItems; i++)
+              {
+                cP.drawImage(goal, endX[i], endY[i], playerSize, playerSize);
+              }
+              cP.drawImage(player, xPlayer, yPlayer, playerSize, playerSize);
           }
         }
-        else
-        {
-            cP.clearRect(0, 0, canvasPlayer.width, canvasPlayer.height);
-            for(var i = 0; i<numberOfItems; i++)
-            {
-              cP.drawImage(goal, endX[i], endY[i], playerSize, playerSize);
-            }
-            cP.drawImage(player, xPlayer, yPlayer, playerSize, playerSize);
+        if(numberOfItems == numberCaught){
+          console.log("win");
+          wonGame = true;
+          cP.clearRect(0, 0, canvasPlayer.width, canvasPlayer.height);
+          cM.clearRect(0, 0, canvasMaze.width, canvasMaze.height);
+          document.getElementById("title").innerHTML = "YOU WON!!! YOU ARE A-MAZE-ING!!!";
+          var winner = new Image();
+          winner.src = "win.png";
+          cP.drawImage(winner,(canvasPlayer.width-winner.width)/2, (canvasPlayer.height-winner.height)/2);
         }
 }
 
 /*Places the goal random in the maze*/
 function randomEnd(){  //0 is end, 1 is player
   for(var i = 0; i<numberOfItems; i++){
-    var maxX =  cP.canvas.width*.5 + (actualMazeWidth*.5);
-    var minX = cP.canvas.width*.5 - (actualMazeWidth * .5);
-    var maxY =  cP.canvas.height*.5 + (actualMazeHeight * .5);
-    var minY =  cP.canvas.height*.5 - (actualMazeHeight * .5);
-
-    tempEndX = Math.floor(Math.random()*(maxX-minX))+minX;
+    tempEndX = Math.floor(Math.random()*(maxX-minX)) + minX;
     tempEndY = Math.floor(Math.random()*(maxY-minY)) + minY;
-
-      while(!canMoveThere(tempEndX,tempEndY))
+      while(!canMoveThereGoal(tempEndX,tempEndY))
       {
-          console.log("run");
-          maxX = cP.canvas.width*.5 + (actualMazeWidth*.5) - sz;
-          minX = cP.canvas.width*.5 - (actualMazeWidth * .5) + sz;
-          maxY =  cP.canvas.height*.5 + (actualMazeHeight * .5) - sz;
-          minY =  cP.canvas.height*.5 - (actualMazeHeight * .5) + sz;
-          console.log(minX +  " to " +  maxX)
-          tempEndX = Math.floor(Math.random()*(maxX-minX))+minX;
-          tempEndY = Math.floor(Math.random()*(maxY-minY)) + minY;
-          console.log(tempEndX);
-          console.log(tempEndY);
+          tempEndX = Math.floor(Math.random() *(maxX-minX))+minX;
+          tempEndY = Math.floor(Math.random() *(maxY-minY)) + minY
       }
 
         endX[i] = tempEndX;
@@ -163,24 +158,45 @@ function lost()
 }
 
 
-/*Returns 1 (can't move) or 0 (can move) based on wheter a player
+/*Returns true or false based on wheter a player
 *can move to any given x and y */
 function canMoveThere(x, y)
 {
-    var collision = true;
+    var canBeThere = true;
     //x > marginX+sz && y > marginY+sz
-    if(x>0 && y>0){
+    if(x > 0 && y > 0){
         var color = cM.getImageData(x, y, playerSize, playerSize);
         var p = color.data;
         var n = p.length;
         for(var i = 0; i<n; i+=4){
             if(p[i] === 0)
             {
-                 collision = false;
+                 canBeThere = false;
             }
         }
       }
-    return collision;
+    return canBeThere;
+}
+
+function canMoveThereGoal(x, y)
+{
+    var canBeThere = true;
+    //x > marginX+sz && y > marginY+sz
+    if(x < maxX - 2*sz && x > minX + sz && y < maxY- 2*sz && y > minY+ sz){
+        var color = cM.getImageData(x, y, playerSize, playerSize);
+        var p = color.data;
+        var n = p.length;
+        for(var i = 0; i<n; i+=4){
+            if(p[i] === 0)
+            {
+                 canBeThere = false;
+            }
+        }
+    }
+    else{
+      canBeThere = false;
+    }
+    return canBeThere;
 }
 
 
@@ -197,10 +213,9 @@ function checkKey(e)
         e.preventDefault();
         for(var i = 0; i<speed; i++)
         {
-            xPlayer = xPlayer--;
-            if(canMoveThere(xPlayer, yPlayer) == 0)
+            xPlayer--;
+            if(canMoveThere(xPlayer, yPlayer))
             {
-
               drawImage();
             }
             else {
@@ -244,13 +259,13 @@ function checkKey(e)
         e.preventDefault();
         for(var i = 0; i<speed; i++)
         {
-            yPlayer--;
+            yPlayer++;
             if(canMoveThere(xPlayer, yPlayer))
             {
               drawImage();
             }
             else {
-              yPlayer++;
+              yPlayer--;
             }
         }
     }
@@ -265,10 +280,10 @@ function checkKey(e)
     }
     if(e.keyCode == minus)
     {
-        speed-= 1;
-        if(speed > minSpeed)
+        speed--;
+        if(speed < minSpeed)
         {
-          speed += 1;
+          speed ++;
         }
     }
 
