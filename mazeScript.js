350690ebc003f04180d1424e44ac51edd2777aef
maxdg99 <Maxdg99@gmail.com>
2018-11-08 19:39:43 -0500

    Timer

--- mazeScript.js
+++ mazeScript.js
@@ -5,3 +5,12 @@ window.onload = function(){
   drawImage("right");
 }
   document.onkeydown = checkKey;
+
+var x = setInterval(function() {
+    var curTime = new Date().getTime();
+    var timeLeft = Math.round((timeLimit - (0.001*(curTime - startTime)))*100)/100;
+    var remaining = numberOfItems - numberCaught;
+    document.getElementById("score").innerHTML = "REMINING GOALS: " + remaining;
+    document.getElementById("time").innerHTML = "TIME LEFT: " + timeLeft;
+    document.getElementById("speed").innerHTML = "SPEED: " + speed;
+}, 100);
